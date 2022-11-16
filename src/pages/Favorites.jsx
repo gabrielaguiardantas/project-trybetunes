ad965b1e2198102975c21840a2655f6e4169171f
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-11-16 19:20:17 -0300

    100% rolando, com bônus e tudo mais

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,68 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs, addSong, removeSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
+import MusicCard from '../components/MusicCard';
 
 class Favorites extends Component {
+  state = {
+    isLoading: false,
+    favoriteSongsList: [],
+  };
+
+  componentDidMount() {
+    this.retrieveFavoriteSongsList();
+  }
+
+  retrieveFavoriteSongsList = async () => {
+    this.setState({ isLoading: true });
+    const retrieveFavorites = await getFavoriteSongs();
+    this.setState({ favoriteSongsList: retrieveFavorites, isLoading: false });
+  };
+
+  favoriteMusicsList = async (music, target) => {
+    this.setState({ isLoading: true });
+    if (target.checked) {
+      await addSong(music);
+      this.setState(({ favoriteSongsList }) => ({
+        favoriteSongsList: [...favoriteSongsList, music],
+      }));
+    } else {
+      await removeSong(music);
+      this.setState({
+        favoriteSongsList: JSON.parse(localStorage.getItem('favorite_songs')),
+      });
+    }
+    this.setState({ isLoading: false });
+  };
+
   render() {
+    const { isLoading, favoriteSongsList } = this.state;
     return (
       <div>
-        <div data-testid="page-favorites">Favorites</div>
-        <Header />
+        {
+          isLoading ? <Loading />
+            : (
+              <div>
+                <div data-testid="page-favorites">Favorites</div>
+                <Header />
+
+                {
+                  favoriteSongsList
+                    .map((music) => (<MusicCard
+                      key={ music.trackId }
+                      music={ music }
+                      trackName={ music.trackName }
+                      trackId={ music.trackId }
+                      previewUrl={ music.previewUrl }
+                      isChecked={ favoriteSongsList
+                        .some((song) => song.trackId === music.trackId) }
+                      favoriteMusicsList={ this.favoriteMusicsList }
+                    />))
+                }
+              </div>
+            )
+        }
       </div>
     );
   }
--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,12 +1,143 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser, updateUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class ProfileEdit extends Component {
+  state = {
+    isLoading: false,
+    isValidProfileEdit: false,
+    redirect: false,
+  };
+
+  componentDidMount() {
+    this.retrieveUserInfo();
+    this.setState({ redirect: false });
+  }
+
+  retrieveUserInfo = async () => {
+    this.setState({ isLoading: true });
+    const retrieveUser = await getUser();
+    const { name, email, image, description } = retrieveUser;
+    this.setState({
+      isLoading: false, name, email, image, description,
+    }, () => this.validateProfileEdit());
+  };
+
+  updateUserInfo = async () => {
+    const { name, email, image, description } = this.state;
+    this.setState({ isLoading: true });
+    await updateUser({ name, email, image, description });
+    this.setState({ isLoading: false, redirect: true });
+  };
+
+  validateProfileEdit = () => {
+    const { name, email, description } = this.state;
+    if (name && email.includes('@')
+    && description) {
+      this.setState({
+        isValidProfileEdit: true,
+      });
+    } else {
+      this.setState({
+        isValidProfileEdit: false,
+      });
+    }
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.validateProfileEdit);
+  };
+
   render() {
+    const { name, image, description,
+      email, isLoading, isValidProfileEdit, redirect } = this.state;
     return (
       <div>
-        <div data-testid="page-profile-edit">ProfileEdit</div>
-        <Header />
+        {
+          isLoading ? <Loading />
+            : (
+              <div>
+                <Header />
+                <div
+                  data-testid="page-profile-edit"
+                >
+                  <br />
+                  <label htmlFor="edit-input-image">
+                    <input
+                      name="image"
+                      id="edit-input-image"
+                      type="text"
+                      alt="edit-image"
+                      onChange={ this.handleChange }
+                      data-testid="edit-input-image"
+                      value={ image }
+                    />
+                  </label>
+                  <br />
+                  <br />
+                  <label htmlFor="edit-input-name">
+                    Nome
+                    <input
+                      type="text"
+                      name="name"
+                      id="edit-input-name"
+                      data-testid="edit-input-name"
+                      value={ name }
+                      onChange={ this.handleChange }
+                    />
+                  </label>
+                  <br />
+                  <br />
+                  <label htmlFor="edit-input-email">
+                    Email
+                    <input
+                      type="email"
+                      name="email"
+                      id="edit-input-email"
+                      data-testid="edit-input-email"
+                      value={ email }
+                      onChange={ this.handleChange }
+                    />
+                  </label>
+                  <br />
+                  <br />
+                  <label htmlFor="edit-input-description">
+                    Descrição:
+                    <br />
+                    <textarea
+                      type="textarea"
+                      name="description"
+                      id="edit-input-description"
+                      value={ description }
+                      rows="8"
+                      cols="20"
+                      onChange={ this.handleChange }
+                      data-testid="edit-input-description"
+                    />
+                  </label>
+                  <br />
+                  <br />
+                  <input
+                    type="button"
+                    disabled={ !isValidProfileEdit }
+                    data-testid="edit-button-save"
+                    value="salvar"
+                    onClick={ this.updateUserInfo }
+                  />
+                  {
+                    redirect && <Redirect to="/profile" />
+                  }
+                </div>
+
+              </div>
+            )
+        }
+
       </div>
     );
   }
